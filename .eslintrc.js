82e61c0b566aeb3bfabeabb1177c70c02a167441
Oussema Ghbara <oussema.ghbara@gmail.com>
2026-04-02 10:53:36 +0100

    chore(infra): ESLint/Prettier config, lockfile, db schema fixes

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: './tsconfig.base.json', sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js'],
+};
